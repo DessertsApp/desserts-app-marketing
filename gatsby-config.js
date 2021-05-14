d897c4f19245d8d0199629c7d9f6e410e36e60a6
Chris Jiang <Christopherjiang@outlook.com>
2021-05-14 14:06:41 -0400

    Initialized Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: "Desserts App Marketing",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
